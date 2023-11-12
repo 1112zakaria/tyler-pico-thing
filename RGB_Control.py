ffe277eb20dde89624f9242205ea1b3a7525f44f
Zakaria Ismail <1112zakaria@gmail.com>
2023-11-12 16:58:12 -0500

    removed LED code

--- RGB_Control.py
+++ RGB_Control.py
@@ -4,10 +4,7 @@ import utime
 import sound
        
 reader = MFRC522(spi_id=0,sck=6,miso=4,mosi=7,cs=5,rst=22)
- 
-red = Pin(0, Pin.OUT)
-green = Pin(1, Pin.OUT)
-blue = Pin(2, Pin.OUT)
+
  
 print("Bring RFID TAG Closer...")
 print("")
@@ -23,19 +20,11 @@ while True:
             
             if card == 111583217:
                 print("Card ID: "+ str(card)+" PASS: Green Light Activated")
-                red.value(0)
-                green.value(1)
-                blue.value(0)
                 sound.play_audio()
                 
             elif card == 495638547:
                 print("Card ID: "+ str(card)+" PASS: Blue Light Activated")
-                red.value(0)
-                green.value(0)
-                blue.value(1)
                 
             else:
                 print("Card ID: "+ str(card)+" UNKNOWN CARD! Red Light Activated")
-                red.value(1)
-                green.value(0)
-                blue.value(0)     
+      
