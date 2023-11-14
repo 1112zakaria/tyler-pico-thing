c6d0f0091c4ebc35106a322f508788d11519bb95
Zakaria Ismail <1112zakaria@gmail.com>
2023-11-14 17:32:15 -0500

    added experimental support for Wav files
    gotta ask tyler if it works before merging to main

--- sound.py
+++ sound.py
@@ -4,23 +4,49 @@ Plays a single MP3 once.
 """
 import board
 import audiomp3
+import audiocore
 import audiopwmio
 import random
 
-def pick_random_song():
-    # add more song files here.
-    song_files = [
-        "slow.mp3",
-    ]
+class SongWrapper():
+    def get_decoder(self):
+        return None
 
-    random_idx = random.randint(0, len(song_files) - 1)
-    return song_files[random_idx]
+class MP3Wrapper(SongWrapper):
+    def __init__(self, path):
+        self.path = path
+    def get_decoder(self):
+        return audiomp3.MP3Decoder(open(self.path, "rb"))
+
+class WavWrapper(SongWrapper):
+    def __init__(self, path):
+        self.path = path
+    def get_decoder(self):
+        return audiocore.WaveFile(open(self.path, "rb"))
+
+
+# Pin Configurations & Other stuff for Tyler
+# If you want to change where you plug stuff in,
+# you can modify the code here
+# You can also update the list of songs, wrap it
+# accordingly whether its MP3 or WAV
+speaker_pin = board.GP13
+wrapped_song_files = [
+    MP3Wrapper("slow.mp3"),
+    WavWrapper("The_Boondocks_I_Want_My_Money.wav")
+]
+# End of pin configurations
+
+
+def pick_random_song():
+    random_idx = random.randint(0, len(wrapped_song_files) - 1)
+    return wrapped_song_files[random_idx]
 
 def play_audio():
-    audio = audiopwmio.PWMAudioOut(board.GP0)
+    audio = audiopwmio.PWMAudioOut(speaker_pin)
     
     random_song = pick_random_song()
-    decoder = audiomp3.MP3Decoder(open(random_song, "rb"))
+    decoder = random_song.get_decoder()
 
     audio.play(decoder)
     while audio.playing:
