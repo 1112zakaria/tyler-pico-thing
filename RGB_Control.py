f37e68b75436f672824637d73ad4879038ee9ec7
Zakaria Ismail <1112zakaria@gmail.com>
2023-11-14 17:20:44 -0500

    current version of tyler's code that allegedly works
    kept here in this branch for tracking

--- RGB_Control.py
+++ RGB_Control.py
@@ -4,7 +4,7 @@ import utime
 import sound
        
 reader = MFRC522(spi_id=0,sck=6,miso=4,mosi=7,cs=5,rst=22)
-
+ 
 print("Bring RFID TAG Closer...")
 print("")
  
@@ -19,11 +19,19 @@ while True:
             
             if card == 111583217:
                 print("Card ID: "+ str(card)+" PASS: Green Light Activated")
+                red.value(0)
+                green.value(1)
+                blue.value(0)
                 sound.play_audio()
                 
             elif card == 495638547:
                 print("Card ID: "+ str(card)+" PASS: Blue Light Activated")
+                red.value(0)
+                green.value(0)
+                blue.value(1)
                 
             else:
                 print("Card ID: "+ str(card)+" UNKNOWN CARD! Red Light Activated")
-      
+                red.value(1)
+                green.value(0)
+                blue.value(0)   
--- sound.py
+++ sound.py
@@ -2,25 +2,14 @@
 CircuitPython single MP3 playback example for Raspberry Pi Pico.
 Plays a single MP3 once.
 """
-import board
 import audiomp3
 import audiopwmio
-import random
-
-def pick_random_song():
-    # add more song files here.
-    song_files = [
-        "slow.mp3",
-    ]
-
-    random_idx = random.randint(0, len(song_files) - 1)
-    return song_files[random_idx]
+import board
 
 def play_audio():
-    audio = audiopwmio.PWMAudioOut(board.GP0)
-    
-    random_song = pick_random_song()
-    decoder = audiomp3.MP3Decoder(open(random_song, "rb"))
+    audio = audiopwmio.PWMAudioOut(board.GP13)
+
+    decoder = audiomp3.MP3Decoder(open("slow.mp3", "rb"))
 
     audio.play(decoder)
     while audio.playing:
