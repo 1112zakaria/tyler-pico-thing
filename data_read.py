7c0d9e879403a7c629fd00c76986d0e2c97f61f6
Zakaria Ismail <1112zakaria@gmail.com>
2023-11-14 22:44:07 -0500

    ported data_read.py to micropython to circuitpython

--- data_read.py
+++ data_read.py
@@ -1,7 +1,13 @@
-from mfrc522 import MFRC522
-import utime
+from cpy_mfrc522 import MFRC522
+import board
  
-reader = MFRC522(spi_id=0,sck=6,miso=4,mosi=7,cs=5,rst=22)
+reader = MFRC522(
+    sck=board.GP6,
+    mosi=board.GP7,
+    miso=board.GP4,
+    rst=board.GP22,
+    cs=board.GP5
+)
  
 print("Bring TAG closer...")
 print("")
@@ -11,8 +17,8 @@ while True:
     reader.init()
     (stat, tag_type) = reader.request(reader.REQIDL)
     if stat == reader.OK:
-        (stat, uid) = reader.SelectTagSN()
-        if stat == reader.OK:
-            card = int.from_bytes(bytes(uid),"little",False)
+        (stat, raw_uid) = reader.anticoll()
+        if reader.select_tag(raw_uid) == reader.OK:
+            card = int.from_bytes(bytes(raw_uid),"little")
             print("CARD ID: "+str(card))
-utime.sleep_ms(500) 
+ 
